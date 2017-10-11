226fe1c7c688089fff797678100b99ad501b7940
runyutang <32315568+runyutang@users.noreply.github.com>
2017-10-11 02:14:17 +0200

    update version

--- sketch.js
+++ sketch.js
@@ -6,18 +6,19 @@ function setup() {
 }
 
 function draw() {
-  strokeWeight(2);
-  stroke(lerpColor(color('#f794a4'),color('#64b3f4'),frameCount/120));
+  strokeWeight(1);
+  stroke(lerpColor(color('#f794a4'),color('#64b3f4'),abs(sin(frameCount))));
   translate(width/2,height/2);
   line(0,0,(cos(frameCount*3)*100-100),(sin(frameCount*3)*100));
   line(0,0,(-(cos(frameCount*3)*100)+100),(-(sin(frameCount*3)*100)));
   line(0,0,((sin(frameCount*3)*100)),(-(cos(frameCount*3)*100))+100);
   line(0,0,(-(sin(frameCount*3)*100)),((cos(frameCount*3)*100))-100);
   
-  fill(0,0,0,10);
-  ellipse(0,0,(frameCount*3+40),(frameCount*3+40));
+  fill(0,0,0,20);
+  noStroke();
+  ellipse(0,0,(abs(sin(frameCount))*800),(abs(sin(frameCount))*800));
   
-    if(frameCount==120){
-    noLoop();
-  }
+  //   if(frameCount==120){
+  //   noLoop();
+  // }
 }
